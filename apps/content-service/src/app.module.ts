fa450235f73c3f21e7942f89d29262ffdefdea55
RChaubey16 <ruturajchaubey16@gmail.com>
2026-04-08 10:49:19 +0530

    feat(content): register DummyModule in AppModule

--- apps/content-service/src/app.module.ts
+++ apps/content-service/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ContentModule } from './content/content.module';
+import { DummyModule } from './dummy/dummy.module';
 
 @Module({
-  imports: [ContentModule],
+  imports: [ContentModule, DummyModule],
 })
 export class AppModule {}
